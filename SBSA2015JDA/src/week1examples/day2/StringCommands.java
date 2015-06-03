b338db8a73721c0065d72583766c4d1a763963e3
Tshepo <motlhodi@gmail.com>
2015-06-03 15:19:11 +0200

    drawLine

---
+++ SBSA2015JDA/src/week1examples/day2/StringCommands.java
@@ -0,0 +1,21 @@
+package week1examples.day2;
+
+public class StringCommands
+{
+
+	public static void main(String[] args)
+	{
+		String s = "   The crazy lady shot the postman.   "; //3 spaces
+		
+		System.out.println(s);
+		
+		drawLine();
+
+	}
+	
+	public static void drawLine()
+	{
+		System.out.println("--------------------------------------");
+	}
+
+}
